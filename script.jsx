86c62b83239468cb3930c0f7150f857f507a8105
Francisco De La Cruz <helloquico@gmail.com>
2015-08-12 17:42:41 -0400

    The Stars, Button, and Answer Frames

--- script.jsx
+++ script.jsx
@@ -1,8 +1,53 @@
+var StarsFrame = React.createClass({
+  render: function() {
+	return (
+	  <div id="stars-frame">
+	  	<div className="well">
+		  <span className="glyphicon glyphicon-star"></span>
+		  <span className="glyphicon glyphicon-star"></span>
+		  <span className="glyphicon glyphicon-star"></span>
+		  <span className="glyphicon glyphicon-star"></span>
+		  <span className="glyphicon glyphicon-star"></span>
+		</div>
+	  </div>
+	);
+  }
+});
+
+var ButtonFrame = React.createClass({
+  render: function() {
+	return (
+	  <div id="button-frame">
+	  	<button className="btn btn-primary">=</button>
+	  </div>
+	);
+  }
+});
+
+var AnswerFrame = React.createClass({
+  render: function() {
+	return (
+	  <div id="answer-frame">
+	  	<div className="well">
+		....
+		</div>
+	  </div>
+	);
+  }
+});
+
 var Game = React.createClass({
   render: function() {
 	return (
 	  <div id="game">
 	  	<h2>Play Nine</h2>
+		<hr />
+
+		<div className="clearfix">
+		  <StarsFrame />
+		  <ButtonFrame />
+		  <AnswerFrame />
+		</div>
 	  </div>
 	);
   }
