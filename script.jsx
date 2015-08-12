ee0b5fc3df00f2c7803869955fbaa28af3e28c7e
Francisco De La Cruz <helloquico@gmail.com>
2015-08-12 16:45:05 -0400

    Take input from the User

--- script.jsx
+++ script.jsx
@@ -19,12 +19,38 @@ var Card = React.createClass({
   }
 });
 
-var Main = React.createClass({
+var Form = React.createClass({
+  handleSubmit: function(e) {
+  	e.preventDefault();
+	var loginInput = React.findDOMNode(this.refs.login);
+	this.props.addCard(loginInput.value);
+	loginInput.value = '';
+  },
   render: function() {
+    return (
+	  <form onSubmit={this.handleSubmit}>
+	  	<input placeholder="github login" ref="login" />
+		<button>Add</button>
+	  </form>
+	);
+  }
+})
+
+var Main = React.createClass({
+  getInitialState: function() {
+  	return {logins: []};
+  },
+  addCard: function(loginToAdd) {
+  	 this.setState({logins: this.state.logins.concat(loginToAdd)})	
+  },
+  render: function() {
+	var cards = this.state.logins.map(function(login) {
+		return (<Card login={login} />);
+	});
 	return (
    	  <div>
-	  	<Card login="spicyj" />
-	  	<Card login="petehunt" />
+	  	<Form addCard={this.addCard} />
+	  	{cards}	
       </div>
 	);
   }
